3c61d60572fd9f1f2b8aaaf4743c0ca6d2a0c37d
mekdes-2022 <120272349+mekdes-2022@users.noreply.github.com>
2023-01-06 16:55:27 -0800

    my apple project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import "./commonResource/css/bootstrap.css";
+import "./commonResource/css/styles.css";
+import Header from './commonResource/Header/Header';
+import Alert from './commonResource/Alert/Alert';
+import IpadPro from './commonResource/SectionOne/IpadPro';
+import MacBookAir from './commonResource/SectionTwo/MacBookAir';
+import Iphone11Pro from './commonResource/SectionThree/Iphone11Pro';
+import Iphone11 from './commonResource/SectionFour/Iphone11';
+import CDC from './commonResource/SectionFive/CDC';
+import Arcade from './commonResource/SectionSix/Arcade';
+import Footer from './commonResource/Footer/Footer';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <Alert />
+      <IpadPro />
+      <MacBookAir />
+      <Iphone11Pro />
+      <Iphone11 />
+      <CDC />
+      <Arcade />
+      <Footer />
     </div>
   );
 }
